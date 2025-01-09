b3001364a5c76dd77574faab1480717f37972d96
Foyzul Karim <foyzulkarim@gmail.com>
2025-01-10 09:32:40 +1100

    Refactor loadtest.js to enhance functionality and add new test cases
    - Introduced a new function `runVllmOpenAI` for testing a different endpoint with customizable parameters.
    - Extracted endpoint, model, and prompt variables for better readability and maintainability.
    - Updated the existing `run` function to use these variables instead of hardcoded values.
    - Added support for additional parameters like `max_tokens` and `temperature` in the new function.

--- loadtest.js
+++ loadtest.js
@@ -1,16 +1,20 @@
 // const autocannon = require('autocannon');
 import autocannon from 'autocannon';
 
+
 const run = async () => {
+  const endpoint = 'http://localhost:11434/api/generate';
+  const model = 'llama3.2';
+  const prompt = 'Why is the sky blue?';
   const instance = autocannon({
-    url: 'http://localhost:11434/api/generate',
+    url: endpoint,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'llama3.2',
-      prompt: 'Why is the sky blue?',
+      model: model,
+      prompt: prompt,
       stream: false
     }),
     connections: 2, // Number of concurrent connections
@@ -18,8 +22,49 @@ const run = async () => {
   });
 
   autocannon.track(instance, { renderProgressBar: true });
-  
+
   await instance;
 };
 
-run();
+const runVllmOpenAI = async () => {
+  /**
+   * curl http://localhost:8000/v1/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+        "model": "facebook/opt-125m",
+        "prompt": "San Francisco is a",
+        "max_tokens": 7,
+        "temperature": 0
+      }'
+
+   */
+
+  const endpoint = 'http://192.168.4.28:8000/v1/completions';
+  const model = 'Qwen/Qwen2.5-Coder-3B-Instruct';
+  const prompt = 'Explain how the internet works';
+  const maxTokens = 700;
+  const temperature = 0;
+
+  const instance = autocannon({
+    url: endpoint,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: model,
+      prompt: prompt,
+      max_tokens: maxTokens,
+      temperature: temperature
+    }),
+    connections: 2, // Number of concurrent connections
+    duration: 15     // Duration of the test in seconds
+  });
+
+  autocannon.track(instance, { renderProgressBar: true });
+
+  await instance;
+
+};
+
+runVllmOpenAI();
